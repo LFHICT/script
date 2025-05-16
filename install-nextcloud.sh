af3fdbe40000d8ab2818e6678e206cf301610f32
Luke Huijssoon <l.huijssoon@student.fontys.nl>
2025-05-16 10:21:43 +0200

    Full setup with storage container

--- install-nextcloud.sh
+++ install-nextcloud.sh
@@ -1,18 +1,59 @@
 #!/bin/bash
 
-# Update en installeer snapd
-apt update
-apt install -y snapd
+# Ophalen van parameters vanuit ARM template (via CustomScript Extension)
+STORAGE_ACCOUNT_NAME=$1
+STORAGE_KEY=$2
+CONTAINER_NAME=$3
 
-# Zorg dat snapd werkt
-systemctl enable snapd
-systemctl start snapd
+echo "=== INSTALLATIE BEGONNEN ==="
+echo "Storage Account: $STORAGE_ACCOUNT_NAME"
+echo "Container Name: $CONTAINER_NAME"
 
-# Installeer nextcloud via snap
+# Updates en vereisten
+apt update -y
+apt install -y snapd unzip curl apache2-utils php-cli php-curl php-xml php-mbstring php-zip php-bz2 php-intl php-gd php-bcmath
+
+# Installatie van Nextcloud via Snap
 snap install nextcloud
 
-# Open de benodigde poorten (optioneel, maar ARM NSG regelt dit al)
-ufw allow 80,443,22/tcp
+# Wacht op voltooiing van initialisatie
+sleep 15
 
-# Restart Nextcloud om zeker te zijn dat alles loopt
-snap restart nextcloud
+# Admin gebruiker aanmaken
+nextcloud.manual-install admin "Luke@2007"
+
+# HTTP activeren (HTTPS optioneel toevoegen)
+nextcloud.enable-https self-signed
+
+# Installatie van Rclone voor het mounten van Azure Blob Storage
+curl https://rclone.org/install.sh | bash
+
+# Rclone configuratiepad
+mkdir -p /root/.config/rclone
+
+cat <<EOF > /root/.config/rclone/rclone.conf
+[azureblob]
+type = azureblob
+account = $STORAGE_ACCOUNT_NAME
+key = $STORAGE_KEY
+endpoint = https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net
+EOF
+
+# Mount de Azure Blob container naar Nextcloud's externe directory via systemd
+mkdir -p /mnt/azureblob
+rclone mount azureblob:$CONTAINER_NAME /mnt/azureblob --daemon --vfs-cache-mode writes
+
+# Externe opslag activeren in Nextcloud
+snap run nextcloud.occ app:enable files_external
+snap run nextcloud.occ app:enable external
+
+# Maak een externe opslagvermelding aan in Nextcloud
+snap run nextcloud.occ files_external:create /AzureBlobStorage local null::local --user=admin
+snap run nextcloud.occ files_external:option /AzureBlobStorage path /mnt/azureblob
+
+# Zorg dat rechten kloppen
+chown -R root:root /mnt/azureblob
+chmod -R 755 /mnt/azureblob
+
+# Klaar!
+echo "=== INSTALLATIE VOLTOOID ==="
